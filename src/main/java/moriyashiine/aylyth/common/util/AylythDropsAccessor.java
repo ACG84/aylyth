0a7e6267a703cbb8d78af39cf5830aba6726e01b
William Guth <139715659+ACG84@users.noreply.github.com>
2025-11-30 12:31:26 -0600

    Create AylythDropsAccessor.java

---
+++ src/main/java/moriyashiine/aylyth/common/util/AylythDropsAccessor.java
@@ -0,0 +1,6 @@
+package moriyashiine.aylyth.common.util;
+
+public interface AylythDropsAccessor {
+    boolean aylyth$shouldPreventDrops();
+    void aylyth$setPreventDrops(boolean preventDrops);
+}
