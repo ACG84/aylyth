756a12748e531fc22724edc075c0193a7619fdc5
William Guth <139715659+ACG84@users.noreply.github.com>
2025-11-30 13:52:35 -0600

    Update AylythEntityLootProvider.java

--- src/datagen/java/moriyashiine/aylyth/datagen/common/loot/AylythEntityLootProvider.java
+++ src/datagen/java/moriyashiine/aylyth/datagen/common/loot/AylythEntityLootProvider.java
@@ -73,4 +73,84 @@ public class AylythEntityLootProvider extends SimpleFabricLootTableProvider {
 
     private LootTable.Builder elderAylythian(EntityType<?> type) {
         return LootTable.builder()
-                .poo
+                .pool(LootPool.builder().with(ItemEntry.builder(Items.BONE)).apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0, 2))).apply(LootingEnchantLootFunction.builder(UniformLootNumberProvider.create(0, 1))))
+                .pool(LootPool.builder().with(ItemEntry.builder(Items.STICK)).apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0, 2))).apply(LootingEnchantLootFunction.builder(UniformLootNumberProvider.create(0, 1))))
+                .pool(LootPool.builder().with(ItemEntry.builder(AylythItems.YMPE_SAPLING)).apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0, 1))).apply(LootingEnchantLootFunction.builder(UniformLootNumberProvider.create(0, 1))))
+                .pool(LootPool.builder().with(ItemEntry.builder(AylythItems.YMPE_FRUIT)).conditionally(KilledByPlayerLootCondition.builder()).conditionally(RandomChanceWithLootingLootCondition.builder(0.25f, 0.01f)))
+                .pool(LootPool.builder()
+                        .with(ItemEntry.builder(AylythItems.WRONGMEAT))
+                        .with(ItemEntry.builder(AylythItems.AYLYTHIAN_HEART))
+                        .conditionally(DamageSourcePropertiesLootCondition.builder(DamageSourcePredicate.Builder.create()
+                                .sourceEntity(EntityPredicate.Builder.create()
+                                        .equipment(EntityEquipmentPredicate.Builder.create()
+                                                .mainhand(ItemPredicate.Builder.create()
+                                                        .tag(AylythItemTags.FLESH_HARVESTERS).build()
+                                                ).build()
+                                        )
+                                )
+                        ))
+                        .conditionally(RandomChanceWithLootingLootCondition.builder(0.2f, 0.0625f))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(3, 5))));
+    }
+
+    private LootTable.Builder scion(EntityType<?> type) {
+        return LootTable.builder()
+                .pool(LootPool.builder().with(GroupEntry.create(ItemEntry.builder(AylythItems.POMEGRANATE), ItemEntry.builder(AylythItems.NYSIAN_GRAPES))))
+                .pool(LootPool.builder().with(ItemEntry.builder(Items.ROTTEN_FLESH).apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 3)))))
+                .pool(LootPool.builder().with(ItemEntry.builder(AylythItems.YMPE_SAPLING)))
+                .pool(LootPool.builder().with(ItemEntry.builder(AylythItems.WRONGMEAT)).conditionally(DamageSourcePropertiesLootCondition.builder(DamageSourcePredicate.Builder.create()
+                        .sourceEntity(EntityPredicate.Builder.create()
+                                .equipment(EntityEquipmentPredicate.Builder.create()
+                                        .mainhand(ItemPredicate.Builder.create()
+                                                .tag(AylythItemTags.FLESH_HARVESTERS).build()
+                                        ).build()
+                                )
+                        )
+                ))
+                        .conditionally(EntityPropertiesLootCondition.builder(LootContext.EntityTarget.THIS, EntityPredicate.Builder.create().typeSpecific(new ScionPredicate(false))))
+                        .conditionally(RandomChanceWithLootingLootCondition.builder(0.15f, 0.0625f))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 2))));
+    }
+
+    private LootTable.Builder wreathedHind(EntityType<?> type) {
+        return LootTable.builder()
+                .pool(LootPool.builder().with(ItemEntry.builder(AylythItems.WRONGMEAT)).conditionally(DamageSourcePropertiesLootCondition.builder(DamageSourcePredicate.Builder.create()
+                                .sourceEntity(EntityPredicate.Builder.create()
+                                        .equipment(EntityEquipmentPredicate.Builder.create()
+                                                .mainhand(ItemPredicate.Builder.create()
+                                                        .tag(AylythItemTags.FLESH_HARVESTERS).build()
+                                                ).build()
+                                        )
+                                )
+                        ))
+                        .conditionally(RandomChanceWithLootingLootCondition.builder(0.2f, 0.0625f))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(3, 5))));
+    }
+
+    private LootTable.Builder ympeMouldLoot(EntityType<?> type) {
+        return LootTable.builder()
+                .pool(LootPool.builder().with(ItemEntry.builder(Items.SOUL_SOIL)).apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 3))));
+    }
+
+    private LootTable.Builder boneflyLoot(EntityType<?> type) {
+        return LootTable.builder()
+                .pool(LootPool.builder().with(ItemEntry.builder(Items.SOUL_SOIL)).apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 3))));
+    }
+
+    private LootTable.Builder tulpaLoot(EntityType<?> type) {
+        return LootTable.builder()
+                .pool(LootPool.builder().with(ItemEntry.builder(Items.SOUL_SOIL)).apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 2))));
+    }
+
+    public <T extends Entity> void addDrop(EntityType<T> type, Function<EntityType<T>, LootTable.Builder> function) {
+        loot.put(type.getLootTableId(), function.apply(type));
+    }
+
+    @Override
+    public void accept(BiConsumer<Identifier, LootTable.Builder> consumer) {
+        this.generateLoot();
+        for (Map.Entry<Identifier, LootTable.Builder> entry : loot.entrySet()) {
+            consumer.accept(entry.getKey(), entry.getValue());
+        }
+    }
+}
